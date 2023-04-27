3aeba5344c30452ff77436cc790c7555b1015ec4
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-04-27 21:45:42 +0300

    MathJax

--- GenerateQuadraticEquationQuestion.js
+++ GenerateQuadraticEquationQuestion.js
@@ -13,16 +13,16 @@ function GenerateQuadraticEquationQuestion() {
     let x1 = GenerateRandomIntNumber();
     let x2 = GenerateRandomIntNumber();
     let question = `<p>Розв'яжіть квадратне рівняння:</p>
-    <p>${createQuadraticEquationUsingVietasFormula(x1, x2)}</p>`;
+    <p>$${createQuadraticEquationUsingVietasFormula(x1, x2)}$</p>`;
     //відповіді містять правильну
-    let correctAnswer = `x1=${x1}, x2=${x2}`;
+    let correctAnswer = `$x_1=${x1}, x_2=${x2}$`;
     let answers = [correctAnswer];
 
     //додаємо ще 3 неправильні відповіді щоб всього їх було 4
     while (answers.length < 4) {
         let x1 = GenerateRandomIntNumber();
         let x2 = GenerateRandomIntNumber();
-        let wrongAnswer = `x1=${x1}, x2=${x2}`;
+        let wrongAnswer = `$x_1=${x1}, x_2=${x2}$`;
         // якщо неправильна відповідь рівна правильній, то пропускаємо її
         if (wrongAnswer == correctAnswer)
             continue;
